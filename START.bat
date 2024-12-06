690be7ca905ea6aa4ce44ee6b91b397c9697094e
A8b1 <discodingpotato@gmail.com>
2024-12-06 11:28:28 +0530

    fix: bat file blah

--- START.bat
+++ START.bat
@@ -1,7 +1,3 @@
-@echo OFF
-:loop
-java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=4 -XX:+AggressiveOpts -Xms2G -Xmx2G -jar spigot.jar nogui
-echo Press control+C to exit the server!
-timeout 3
-echo (%time%) Restarting!
-goto loop
+@echo off
+java -server -XX:ParallelGCThreads=4 -Xms2G -Xmx2G -jar spigot.jar nogui
+pause
